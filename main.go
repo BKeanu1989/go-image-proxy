6d0cb4dcd71742e73d51ecbd24cafb7db60c6778
Kevin Fechner <kevin@mokka-merch.com>
2024-04-20 16:58:20 +0200

    init image proxy - as absolute beginner

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+// TODO: key & salt for signing
+// http server
+// quality, width, height, convert
+
+// sha?
+// cache - fs &/ memory => hash map[hash of filename] = base64
+
+// wordpress shortcode für bilder? mit js für dynamische größen
+
+func main() {
+
+}
